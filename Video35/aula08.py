f86033f4916f4fbbc1aa3d68c846749ffd199d05
Diego <diego.aoas@gmail.com>
2022-05-26 23:38:22 -0300

    Revert "Correção"
    This reverts commit 61df2741c4528af4289e4d157501204f9e87e6ee.

--- Video35/aula08.py
+++ Video35/aula08.py
@@ -13,17 +13,3 @@ Acumuladores
 #
 #     acumulador = acumulador + contador
 #     contador += 1
-
-### EXEMPLO 2:
-
-contador = 1
-acumulador = 1
-
-while contador <= 10:
-    print(contador, acumulador)
-
-    acumulador = acumulador + contador
-    contador += 1
-else:
-    print('Cheguei no else.')
-print('Isso será executado.')
--- Video37/exemplo_5.py
+++
@@ -1,9 +0,0 @@
-"""
-For in em Python
-Iterando strings com for
-Função range(start=0, stop, step=1)
-"""
-texto = 'Python'
-
-for letra in texto:
-    print(letra)
