64fe6edfd6db3ae4422bb3c1f9a1a707f6b32197
Diego <diego.aoas@gmail.com>
2022-06-10 23:27:07 -0300

    Atualização

--- Video42/enumerate_tiraduvidas.py
+++ Video42/enumerate_tiraduvidas.py
@@ -1,3 +1,18 @@
 """
 * Enumerate - Enumerar elementos da lista # list
 """
+
+lista = [
+    # 0      1        2
+    ['Edu', 'João', 'Luiz'],  # 0
+    ['Maria', 'Aline', 'Joana'],  # 1
+    ['Helena', 'Ed', 'Lu']  # 2
+]
+
+# print(lista[1][2])
+
+# enumerada = enumerate(lista)
+# print(list(enumerada))
+
+enumerada = list(enumerate(lista))
+print((enumerada[1][1][2]))
