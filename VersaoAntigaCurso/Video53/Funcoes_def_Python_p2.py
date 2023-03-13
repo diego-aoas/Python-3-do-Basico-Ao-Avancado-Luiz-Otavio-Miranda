a1bd1780746ca92a4edec58d4fca610da9adb5ac
Diego <diego.aoas@gmail.com>
2023-03-12 22:20:53 -0300

    Atualização do Curso na Udemy!

---
+++ VersaoAntigaCurso/Video53/Funcoes_def_Python_p2.py
@@ -0,0 +1,15 @@
+"""
+Funções - def em Python (Parte 2)
+"""
+
+
+def funcao(var):
+    return var
+
+
+variavel = funcao('Valor que eu quero')
+
+if variavel:
+    print(variavel)
+else:
+    print('Nenhum valor')
