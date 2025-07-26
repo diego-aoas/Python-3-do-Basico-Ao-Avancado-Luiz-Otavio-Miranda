f1556060158ca7fd8423e5b291b887e0f6cd2e50
Diego <diego.aoas@gmail.com>
2025-07-26 00:46:38 -0300

    Atualização

---
+++ 2025/Aulas/aula20-int_float.py
@@ -0,0 +1,11 @@
+print(11)
+print(-11)
+print(0)
+
+print(1.1, 10.11)
+print(0.0, -1.5)
+
+# Função type mostra o tipo que o Python inferiu ao valor.
+print(type('Otávio'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))
--- 2025/aula20-int_float.py
+++
@@ -1,7 +0,0 @@
-print(11)
-print(-11)
-print(0)
-
-print(1.1, 10.11)
-print(0.0, -1.5)
-
